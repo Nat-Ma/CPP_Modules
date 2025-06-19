90ce1b9ac5ce05a67cec578a97f55bd63048035d
Natalie Rauh <nrauh@student.42berlin.de>
2025-06-19 14:57:55 +0200

    change read and write filenames

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: natalierauh <natalierauh@student.42.fr>    +#+  +:+       +#+        */
+/*   By: nrauh <nrauh@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 10:05:06 by natalierauh       #+#    #+#             */
-/*   Updated: 2025/06/14 12:20:10 by natalierauh      ###   ########.fr       */
+/*   Updated: 2025/06/19 14:56:24 by nrauh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,29 @@
 
 int	main(int argc, char **argv) {
 	std::string		argStr = argv[2];
-	std::string 	buffer;
+	std::string		buffer;
 	size_t			pos;
 
 	if (argc != 4)
 		return (1);
 	const std::string &s1 = argStr;
-	std::ifstream MyReadFile(argv[1]);
-	if (MyReadFile.fail()) {
+	std::ifstream ReadFile(argv[1]);
+	if (ReadFile.fail()) {
 		std::cout << "File not found" << std::endl;
 		return (1);
 	}
-	std::ofstream MyFile("newfile.txt");
-	while (getline (MyReadFile, buffer)) {
+	std::ofstream WriteFile("newfile.txt");
+	while (getline (ReadFile, buffer)) {
 		pos = buffer.find(s1);
 		if (pos == std::string::npos)
-			MyFile << buffer;
+			WriteFile << buffer;
 		else {
-			MyFile << buffer.substr(0, pos);
-			MyFile << argv[3];
-			MyFile << buffer.substr(pos + s1.size(), buffer.size());
+			WriteFile << buffer.substr(0, pos);
+			WriteFile << argv[3];
+			WriteFile << buffer.substr(pos + s1.size(), buffer.size());
 		}
-		MyFile << "\n";
+		WriteFile << "\n";
 	}
-	MyFile.close();
-	MyReadFile.close();
+	WriteFile.close();
+	ReadFile.close();
 }
